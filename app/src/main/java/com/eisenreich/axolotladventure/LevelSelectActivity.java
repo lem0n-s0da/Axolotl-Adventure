c4014607ff4652ca281f9bcc87efc6fb205afc5c
Alex Eisenreich <aeisenreich7@gmail.com>
2025-05-27 14:16:48 -0400

    worked on Level progress screen

--- app/src/main/java/com/eisenreich/axolotladventure/LevelSelectActivity.java
+++ app/src/main/java/com/eisenreich/axolotladventure/LevelSelectActivity.java
@@ -1,6 +1,10 @@
 package com.eisenreich.axolotladventure;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +17,7 @@ public class LevelSelectActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_level_select);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -20,5 +25,22 @@ public class LevelSelectActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button levelButton;
+        for (int i = 1; i <= 3; i++) {
+            if (i == 3) {
+                levelButton = findViewById(R.id.button_level3);
+            } else if (i == 2) {
+                levelButton = findViewById(R.id.button_level2);
+            } else {
+                levelButton = findViewById(R.id.button_level1);
+            }
+            final int level = i;
+            levelButton.setOnClickListener(v -> {
+                Intent intent = new Intent(this, GameActivity.class);
+                intent.putExtra("level", level);
+                startActivity(intent);
+            });
+        }
     }
 }
